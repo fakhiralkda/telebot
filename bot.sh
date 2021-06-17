c223d3708776be481acebfbc5c1f38cd403f9d39
dαvιѕтα <rzlamrr.dvst@protonmail.com>
2021-06-18 06:01:02 +0700

    Update bot.sh

--- bot.sh
+++ bot.sh
@@ -1,9 +1,9 @@
 install() {
   apt update && apt install -qqy --no-install-recommends git apt-transport-https ca-certificates curl gnupg lsb-release
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
   echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
   apt update -qqy
   apt install -qqy --no-install-recommends docker-ce docker-ce-cli containerd.io
 }
