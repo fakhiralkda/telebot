b3b9078a1b6114d646826708fd686cb7e8f31976
dαvιѕтα <rzlamrr.dvst@protonmail.com>
2021-06-18 06:03:22 +0700

    Update bot.sh

--- bot.sh
+++ bot.sh
@@ -6,6 +6,7 @@ install() {
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
   apt update -qqy
   apt install -qqy --no-install-recommends docker-ce docker-ce-cli containerd.io
+  systemctl start docker
 }
 
 main() {
