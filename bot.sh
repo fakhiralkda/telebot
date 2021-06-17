8fae22dd1d91c1cdd05e2f13c914f0b9cca94f63
dαvιѕтα <rzlamrr.dvst@protonmail.com>
2021-06-18 06:09:54 +0700

    Update bot.sh

--- bot.sh
+++ bot.sh
@@ -1,19 +1,19 @@
 install() {
   apt update && apt install -qqy --no-install-recommends git apt-transport-https ca-certificates curl gnupg lsb-release
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-  echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-  apt update -qqy
-  apt install -qqy --no-install-recommends docker-ce docker-ce-cli containerd.io
-  systemctl start docker
+  #curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+  #echo \
+  #"deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  #$(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+  #apt update -qqy
+  #apt install -qqy --no-install-recommends docker-ce docker-ce-cli containerd.io
+  #systemctl start docker
 }
 
 main() {
   git clone $GIT_REPO_PRIV -b slam-new bot
   cd bot || exit
   docker build . -t bot
-  docker run bot
+  docker run --privileged bot:latest
 }
 
 install > /dev/null
