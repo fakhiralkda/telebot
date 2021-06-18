2f0a6478e2783d2dd35cb7ffc5e6c426bc3fd2db
dαvιѕтα <rzlamrr.dvst@protonmail.com>
2021-06-18 15:57:55 +0700

    logging: critical

--- bot.sh
+++ bot.sh
@@ -1,7 +1,11 @@
 main() {
-  sudo apt update && sudo apt install git
+  sudo apt update && sudo apt install -y -q --no-install-recommends git
   git clone $GIT_REPO_PRIV -b slam-new bot
   cd bot || exit
+  sed -i 's/level\=logging\.INFO/level\=logging\.CRITICAL/g' bot/__init__.py
+  sudo apt -y clean && sudo apt -y autoremove
+  sudo rm -rf /var/lib/apt/lists
+  sudo rm -rf /var/cache/apt/archives
   docker build . -t bot
   docker run bot
 }
